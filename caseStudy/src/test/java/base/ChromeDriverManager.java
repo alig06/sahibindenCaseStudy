615d4dc5021c7f5339550e17642367ed521ae3cd
alig06 <ali.gkts.14@gmail.com>
2019-07-21 13:18:44 +0300

    .

--- caseStudy/src/test/java/base/ChromeDriverManager.java
+++ caseStudy/src/test/java/base/ChromeDriverManager.java
@@ -4,8 +4,4 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeDriverManager {
     protected static ChromeDriver driver = new ChromeDriver();
-
-    public static ChromeDriver getDriver() {
-        return driver;
-    }
 }
